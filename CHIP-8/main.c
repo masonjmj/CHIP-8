ce8f1c5a918d1eacf788f1771a7a94b408b08e63
James Mason <jamesmjmason@icloud.com>
2022-08-17 14:17:21 +0100

    Initial Commit

---
+++ CHIP-8/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CHIP-8
+//
+//  Created by James Mason on 2022-08-17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
